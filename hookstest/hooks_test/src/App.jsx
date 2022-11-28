6c3179d17b75a2f9d50238a0e64e38e72d4104de
Vaše Jméno <vilem.sobotka@gmail.com>
2022-11-28 14:42:42 +0100

    hodina

---
+++ hookstest/hooks_test/src/App.jsx
@@ -0,0 +1,62 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+const delay = (ms) => {
+  console.log("start delay");
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms)
+  });
+};
+
+function App() {
+  const [loading, setLoading] = useState(false)
+
+  const [count, setCount] = useState(0)
+
+  const handleOnClick = async () => {
+    setLoading(true)
+    console.log("Lobo");
+    await delay(5000)
+    setCount((count) => count + 1);
+    console.log("tomie")
+    setLoading(false)
+  };
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => handleOnClick()}>
+          count is {count}
+        </button>
+        <button onClick={() => setCount(0)}>
+          vymazat
+        </button>
+        <button onClick={() => setCount((count) => count - 1)}>
+          odečíst
+        </button>
+        <button onClick={() => setCount((count) => count*count + count*count*count)}>
+          leporelo 
+        </button>
+        <p>{loading ? "Loading" : null}</p>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
+}
+
+export default App
