d124d96c4e41061c9f52078631d2212c7f0eee58
Vaše Jméno <vas@email.com>
2023-01-30 15:19:54 +0100

    import export teleport

--- tutorial-router/src/main.jsx
+++ tutorial-router/src/main.jsx
@@ -4,13 +4,25 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import ErrorPage from "./routes/error-page";
 import "./index.css";
+import Root from "./routes/root";
+import Contact from "./routes/contact";
+import { Children } from "react";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+    path: "contacts/:contactId",
+    element: <Contact />
+    }
+    ]
   },
+  
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
