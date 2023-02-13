88a7f4f7346fd11e67b82edb414c701454e44a8d
Vaše Jméno <vas@email.com>
2023-02-13 15:16:49 +0100

    hodina router

--- tutorial-router/src/main.jsx
+++ tutorial-router/src/main.jsx
@@ -6,23 +6,44 @@ import {
 } from "react-router-dom";
 import ErrorPage from "./routes/error-page";
 import "./index.css";
-import Root from "./routes/root";
-import Contact from "./routes/contact";
+import Root, { loader as rootLoader, action as rootAction } from "./routes/root";
+import Contact, { loader as contactLoader } from "./routes/contact";
 import { Children } from "react";
+import EditContact, { action as editAction } from "./routes/edit";
+import { action as destroyAction } from "./routes/destroy";
+import Index from "./routes";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
     children: [
       {
-    path: "contacts/:contactId",
-    element: <Contact />
-    }
+        index: true,
+        element: <Index />
+      },
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+        loader: contactLoader
+      },
+      {
+        path: "contacts/:contactId/destroy",
+        action: destroyAction,
+        errorElement: <ErrorPage />
+      },
     ]
   },
-  
+  {
+    path: "contacts/:contactId/edit",
+    element: <EditContact />,
+    action: editAction,
+    loader: contactLoader,
+    errorElement: <ErrorPage />
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
