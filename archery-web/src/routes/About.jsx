7b14c8af098412604247474a22b2811c2876e080
Vilém Sobotka <vilem.sobotka@gmail.com>
2023-03-19 20:57:24 +0100

    abot page

--- archery-web/src/routes/About.jsx
+++ archery-web/src/routes/About.jsx
@@ -1,16 +1,17 @@
+import { ReactSVG } from "react-svg"
 function About(){
     return(
         <div className="flex flex-row">
             <div>
-                <h1 className="text-target-blue">Sportovní lukostřelba</h1>
-                <p className="text-target-light-blue">
+                <h1 className="text-target-blue text-2xl">Sportovní lukostřelba</h1>
+                <p className="text-target-light-blue w-96">
                 Sportovní lukostřelba je disciplína, která vyžaduje precizní zaměření a zručnost. 
                 Závodníci se snaží zasáhnout terč co nejpřesnějším šípem z luku. Tato sportovní disciplína vyžaduje nejen fyzickou, 
                 ale i psychickou sílu, trpělivost a koncentraci. Závodníci soutěží v různých kategoriích, 
                 jako jsou individuální a týmové závody, a mohou se zúčastnit mezinárodních turnajů. 
                 Pro mnoho lidí je lukostřelba nejen sportem, ale také relaxační aktivitou, která pomáhá zbavit se stresu a zlepšit soustředění.</p>
             </div>
-            <img src="../assets/target.svg" alt="terč" />
+            <ReactSVG src="../assets/target.svg" />
         </div>
     )
 }
--- archery-web/src/routes/Root.jsx
+++ archery-web/src/routes/Root.jsx
@@ -1,24 +1,29 @@
 import { Outlet } from "react-router-dom";
+import { ReactSVG } from "react-svg";
 
 import NavLink from "../NavLink";
 export default function Root() {
     return (
-        <div className="flex flex-col">
-            <nav class="absolute inset-x-0 top-0 flex flex-col md:flex-row bg-blue-600 text-white shadow-md leading-none">
-                <div class="flex items-center mx-5 py-5 md:py-0">
-                    <img src="../assets/bow.svg" alt="luk"/>
-                    <h1 class="text-target-blue font-bold text-3xl ml-2 inline-block">Brand</h1>
-                </div>
-                <div class="md:flex md:flex-grow text-target-blue">
-                    <ul class="text-lg md:flex md:ml-auto ">
-                        <li><NavLink text="Úvod" link=""/></li>
-                        <li><NavLink text="Informace" link=""/></li>
-                        <li><NavLink text="Jak začít" link=""/></li>
-                        <li><NavLink text="Závody" link=""/></li>
-                    </ul>
-                </div>
-            </nav>
-            <Outlet className="py-5" />
+        <div className="grid grid-cols-1 gap-64">
+            <div>
+                <nav class="absolute inset-x-0 top-0 flex flex-col md:flex-row bg-blue-600 text-white shadow-md leading-none">
+                    <div class="flex items-center mx-5 py-5 md:py-0">
+                        <ReactSVG src="../assets/bow.svg"/>
+                        <h1 class="text-target-blue font-bold text-3xl ml-2 inline-block">Lukostřelba</h1>
+                    </div>
+                    <div class="md:flex md:flex-grow text-target-blue">
+                        <ul class="text-lg md:flex md:ml-auto ">
+                            <li><NavLink text="Úvod" link="About" /></li>
+                            <li><NavLink text="Informace" link="" /></li>
+                            <li><NavLink text="Jak začít" link="" /></li>
+                            <li><NavLink text="Závody" link="" /></li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+            <div>
+                <Outlet />
+            </div>
         </div>
     );
 }
